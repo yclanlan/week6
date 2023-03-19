4369f30405c3902eba9139b21ca6cdaa27748e54
Lan <lanlan.yichun@gmail.com>
2023-03-19 11:03:52 -0400

    new function, clean variable ,rename variable

--- sketch.js
+++ sketch.js
@@ -3,62 +3,55 @@ let  y1 = 0;
 let  x2 = 0;
 let  y2 = 0;
 
-let easing1=0.1
-let easing2=0.05
+let easing1 =0.01;
+let easing2 =0.1;
 
 function setup() {
-  
+
     createCanvas(windowWidth, windowHeight);
     background(0);
 
 }
 
 function draw() {
-
-    background(0)
+    background(0);
   
-    //location
+    //print mouse location
+    fill(255);
     textSize(15)
     text(int(mouseX)+","+int(mouseY),50,50)
-
-    let targetX1 = mouseX;
-    x1 = x1 + (targetX1 - x1) * easing1;
-    let targetY1 = mouseY;
-    y1 = y1 + (targetY1 - y1) * easing1;
-  
-    let targetX2 = mouseX;
-    x2 = map(mouseX, 0, windowWidth, x1 - 80, x1 + 80);
-    x2 = x2 + (targetX2 - x2) * easing1;
-    let targetY2 = mouseY;
-    y2 = map(mouseY, 0, windowWidth, y1 - 60, y1 + 60);
-    y2 = y2 + (targetY2 - y2) * easing1;
-  
-    print(mouseX, mouseY);
     translate(-300, -250);
-   
-    // head
-    fill(255);
-    noStroke();
-    ellipse(x1 + 300, y1 + 200, 200, 200);
+
+    let currentMouseX = mouseX;
+    let currentMouseY = mouseY;
+
+    x1+= (currentMouseX - x1) * easing1;
+    y1+= (currentMouseY - y1) * easing1;
+
+    x2 = map(mouseX, 0, windowWidth, x1 - 80, x1 + 80);
+    x2+= (currentMouseX - x2) * easing2;
     
-    //shodow
-    fill(240);
+    y2 = map(mouseY, 0, windowWidth, y1 - 60, y1 + 60);
+    y2+= (currentMouseY - y2) * easing2;
+  
     noStroke();
-    ellipse(x1 + 305, y1 + 220, 160, 160);
+
+    // head
+    drawCircle(255, x1 + 300, y1 + 200, 200);
+
+    // shodow
+    drawCircle(240, x1 + 305, y1 + 220, 160,);
 
     // eyes
-    fill(30);
-    noStroke();
-    ellipse(x2 + 265, y2 + 210, 13, 13);
-    fill(30);
-    noStroke();
-    ellipse(x2 + 335, y2 + 210, 13, 13);
- 
- 
-  
+    drawCircle(30, x2 + 265, y2 + 210, 13);
+    drawCircle(30, x2 + 335, y2 + 210, 13);
 
-  
 }
 
- 
+function drawCircle(color, x, y, size){
+    fill(color);
+    noStroke();
+    // ellipse(x, y, w, [h])
+    ellipse(x,y,size);
+}
 
